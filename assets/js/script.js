1e969c45c39b075fe5180cdd005f15debcd07628
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 08:25:58 +0000

    refactor: remove event listener from game display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
 let gameDisplay = document.querySelector('#game-display')
 
-gameDisplay.addEventListener('click', function(e){
-    console.log(e)
-})
-//.trim() function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+//.trim() method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
 const game = {
     time: document.querySelector('#time').innerHTML.trim(),
     score: document.querySelector('#score').innerHTML.trim()
 }
-
+//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
 function countDown(){
     document.querySelector('#time').innerHTML = `${game.time--}`
 }
