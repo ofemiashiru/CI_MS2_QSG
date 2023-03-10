337ee08536c4b93263304452df779724c26f45cd
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 08:53:33 +0000

    refactor: update game object over property

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,20 @@ let gameDisplay = document.querySelector('#game-display')
 const game = {
     time: document.querySelector('#time').innerHTML.trim(),
     score: document.querySelector('#score').innerHTML.trim(),
-    over: false
+    isOver: false
 }
 
 //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
 //clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
 function countDown(){
     
-    if(!game.over){
+    if(!game.isOver){
         document.querySelector('#time').innerHTML = `${game.time--}`
 
         if(game.time === -1){ // changed to minus one as 0 displays 1 on timer 
-            game.over = true
+            game.isOver = true
         }
+
     } else {
         clearInterval(timer)
         console.log('game over')
