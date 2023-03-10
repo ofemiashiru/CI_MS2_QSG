3c77138c84ab6cfdc13e5caaa02c917ee656dd30
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 11:55:30 +0000

    feat: start on generating random targets

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,60 +2,63 @@ const game = {
     time: 10,
     score: 0,
     bullets: 3,
-    isOver: false
-}
+    isOver: false,
+    targets: ['good', 'bad', 'addBullet']
+};
 
 
 let gameDisplay = document.querySelector('#game-display')
 
 //Setup new Game --
-document.querySelector('#time').innerHTML = game.time
-document.querySelector('#score').innerHTML = game.score
-document.querySelector('#bullets').innerHTML = game.bullets
+document.querySelector('#time').innerHTML = game.time;
+document.querySelector('#score').innerHTML = game.score;
+document.querySelector('#bullets').innerHTML = game.bullets;
+
+let generatedTarget = game.targets[Math.floor(Math.random() * 3)]
+console.log(generatedTarget)
 
 //Countdown -
-//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
 function countDown(){
     
     if(!game.isOver){
-        document.querySelector('#time').innerHTML = `${--game.time}`// moved -- before variable to action decrement first 
+        document.querySelector('#time').innerHTML = `${--game.time}`;// moved -- before variable to action decrement first 
         //document.querySelector('#score').innerHTML = `${game.score+= 2}`
 
         if(game.time === 0){ 
-            game.isOver = true
+            game.isOver = true;
         }
 
     } else {
 
-        gameOver()
+        gameOver();
 
     }
 }
-
-let timer = setInterval(countDown, 1000)
+//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
+let timer = setInterval(countDown, 1000);
 
 //Use Bullet --
 function fireBullet(){
-    document.querySelector('#bullets').innerHTML = `${--game.bullets}`
+    document.querySelector('#bullets').innerHTML = `${--game.bullets}`;
     if(game.bullets === 0){
 
-        game.isOver = true
+        game.isOver = true;
 
     }
 
     if(game.isOver){
-        gameOver()
+        gameOver();
     }
 }
-gameDisplay.addEventListener('click', fireBullet)
+gameDisplay.addEventListener('click', fireBullet);
 
 //Game Over --
 function gameOver(){
     //clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
-    clearInterval(timer)
+    clearInterval(timer);
 
     //removeEventListener https://www.w3schools.com/jsref/met_document_removeeventlistener.asp
-    gameDisplay.removeEventListener('click', fireBullet)
+    gameDisplay.removeEventListener('click', fireBullet);
 
-    console.log('game over')
+    console.log('game over');
 }
