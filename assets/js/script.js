0bc068c9741b38ba5c88999ed82a41d7f7d0b8c6
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 08:34:10 +0000

    feat: update timer to stop on 0 and console log game over

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,24 @@ let gameDisplay = document.querySelector('#game-display')
 //.trim() method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
 const game = {
     time: document.querySelector('#time').innerHTML.trim(),
-    score: document.querySelector('#score').innerHTML.trim()
-}
-//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
-function countDown(){
-    document.querySelector('#time').innerHTML = `${game.time--}`
+    score: document.querySelector('#score').innerHTML.trim(),
+    over: false
 }
 
-setInterval(countDown, 1000)
+//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
+//clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
+function countDown(){
+    
+    if(!game.over){
+        document.querySelector('#time').innerHTML = `${game.time--}`
+
+        if(game.time === -1){ // changed to minus one as 0 displays 1 on timer 
+            game.over = true
+        }
+    } else {
+        clearInterval(timer)
+        console.log('game over')
+    }
+}
+
+let timer = setInterval(countDown, 1000)
