4550a6dc8b289dd2f8200e8d259d507740827f3e
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 11:07:25 +0000

    feat: add event listener to use bullets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,48 @@
-let gameDisplay = document.querySelector('#game-display')
-
 const game = {
     time: document.querySelector('#time').innerHTML,
     score: 0,
+    bullets: document.querySelector('#bullets').innerHTML,
     isOver: false
 }
+// $(document).ready(function() {
 
-//setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
-//clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
-function countDown(){
+    let gameDisplay = document.querySelector('#game-display')
+
+
+
     
-    if(!game.isOver){
-        document.querySelector('#time').innerHTML = `${game.time--}`
-        document.querySelector('#score').innerHTML = `${game.score+= 2}`
- 
-        if(game.time === -1){ // changed to minus one as 0 displays 1 on timer 
+    //Use Bullet
+    gameDisplay.addEventListener('click', function(e){
+        
+        if(game.bullets === 0){
+
             game.isOver = true
+
+        } else {
+
+            document.querySelector('#bullets').innerHTML = `${--game.bullets}`
+            
         }
+    }, false)
 
-    } else {
-        clearInterval(timer)
-        console.log('game over')
+    //Countdown
+    //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
+    //clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
+    function countDown(){
+        
+        if(!game.isOver){
+            document.querySelector('#time').innerHTML = `${--game.time}`// moved -- before variable to action decrement first 
+            //document.querySelector('#score').innerHTML = `${game.score+= 2}`
+    
+            if(game.time === 0){ 
+                game.isOver = true
+            }
+
+        } else {
+            clearInterval(timer)
+            console.log('game over')
+        }
     }
-}
 
-let timer = setInterval(countDown, 1000)
+    let timer = setInterval(countDown, 1000)
+// })
