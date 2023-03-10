9e065048b91339d12772d7abdbbc733f2fccd752
ofemiashiru <ofemiashiru@gmail.com>
2023-03-10 08:24:30 +0000

    feat: add game timer/ add game properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,14 @@ let gameDisplay = document.querySelector('#game-display')
 gameDisplay.addEventListener('click', function(e){
     console.log(e)
 })
+//.trim() function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+const game = {
+    time: document.querySelector('#time').innerHTML.trim(),
+    score: document.querySelector('#score').innerHTML.trim()
+}
 
+function countDown(){
+    document.querySelector('#time').innerHTML = `${game.time--}`
+}
+
+setInterval(countDown, 1000)
