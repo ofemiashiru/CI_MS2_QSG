44bb81d419f7c45acf95afef53b653efc67f1b74
ofemiashiru <ofemiashiru@gmail.com>
2023-03-19 20:27:51 +0000

    feat: create nav bar for homepage

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector('#nav-button');
+
+navButton.addEventListener('click', function(){
+    document.querySelector('#my-nav').classList.toggle('show-nav')
+
+});
