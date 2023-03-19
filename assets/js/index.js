c9a4f7f4b25daf5f026a7575ef72f4f5f1007228
ofemiashiru <ofemiashiru@gmail.com>
2023-03-19 21:49:01 +0000

    feat: smooth scrolling/prep index with sections/ add js to close nav when link clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 const navButton = document.querySelector('#nav-button');
 
-navButton.addEventListener('click', function(){
-    document.querySelector('#my-nav').classList.toggle('show-nav')
+navButton.addEventListener('click', () => {
+    document.querySelector('#my-nav').classList.toggle('show-nav');
 
+});
+
+const allNavLinks = document.querySelectorAll('#my-nav a');
+
+allNavLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        document.querySelector('#my-nav').classList.remove('show-nav');
+    });
 });
