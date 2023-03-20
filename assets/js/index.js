fa4e38ceb7338b1573ada2758b40eac2d98e1a2a
ofemiashiru <ofemiashiru@gmail.com>
2023-03-20 17:42:10 +0000

    feat: add close icon and update index.js to show/hide close and burger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,49 @@
-const navButton = document.querySelector('#nav-button');
+// handles the burger menu on home page
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
 
-navButton.addEventListener('click', () => {
-    document.querySelector('#my-nav').classList.toggle('show-nav');
+
+//when burger menu clicked show menu and show close button
+openMenu.addEventListener('click', () => {
+    document.querySelector('#my-nav').classList.add('show-nav');
+    openMenu.classList.remove('show');
+    openMenu.classList.add('hide');
+    closeMenu.classList.remove('hide');
+    closeMenu.classList.add('show');
 
 });
 
+// when close is clicked close menu and show burger menu
+closeMenu.addEventListener('click', () => {
+    document.querySelector('#my-nav').classList.remove('show-nav');
+    openMenu.classList.remove('hide');
+    openMenu.classList.add('show');
+    closeMenu.classList.remove('show');
+    closeMenu.classList.add('hide');
+
+});
+
+// closes menu when individual link is clicked
 const allNavLinks = document.querySelectorAll('#my-nav a');
 
 allNavLinks.forEach((link) => {
     link.addEventListener('click', () => {
         document.querySelector('#my-nav').classList.remove('show-nav');
+        openMenu.classList.remove('hide');
+        openMenu.classList.add('show');
+        closeMenu.classList.remove('show');
+        closeMenu.classList.add('hide');
     });
+});
+
+// closes menu when on a bigger screen and resets to burger menu
+window.addEventListener('resize', () => {
+    if(window.screen.width > 1093){
+        document.querySelector('#my-nav').classList.remove('show-nav');
+        openMenu.classList.remove('hide');
+        openMenu.classList.add('show');
+        closeMenu.classList.remove('show');
+        closeMenu.classList.add('hide');
+
+    }
 });
