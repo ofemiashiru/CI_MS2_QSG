479d0c7c6c28230ff32b63ffa4aef6b6515ced66
ofemiashiru <ofemiashiru@gmail.com>
2023-03-21 21:38:39 +0000

    feat: allow game to store high score in local storage

--- assets/js/highScore.js
+++ assets/js/highScore.js
@@ -1,7 +1,13 @@
+localStorage.clear()
 
 const submitHighScore = (form) =>{
-    console.log(form.gamer.value)
-    console.log(form.finalScore.value);
+    let gamerName = form.gamer.value;
+    let gamerScore = form.finalScore.value;
+    let newScore = {[gamerName]:gamerScore};
+    //stop code overwriting localStorage https://stackoverflow.com/questions/40843773/localstorage-keeps-overwriting-my-data
+    let highScores = JSON.parse(localStorage.getItem("newScore")) || [];
+    highScores.push(newScore);
+    localStorage.setItem("newScore", JSON.stringify(highScores));
 
     return false
 }
