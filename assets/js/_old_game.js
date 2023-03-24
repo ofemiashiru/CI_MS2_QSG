4bd90362aac6f27ca6665eec70b4320c38b33e26
ofemiashiru <ofemiashiru@gmail.com>
2023-03-24 18:00:52 +0000

    docs: swap the refactor code with newly created game.js script

---
+++ assets/js/_old_game.js
@@ -0,0 +1,257 @@
+// New game Object
+const game = {
+    isOver: false
+};
+
+// get the gameDisplay and store it as a variable
+const gameDisplay = document.querySelector('#game-display');
+//location of all targets on DOM
+const allTargets = document.querySelector('#all-targets');
+
+// Get X and Y axis totals for game display
+let gameDisplayXAxis = gameDisplay.clientWidth - 20;
+let gameDisplayYAxis = gameDisplay.clientHeight - 20;
+
+//update gameDisplay size when user resizes screen
+window.addEventListener('resize', function(){
+    gameDisplayXAxis = gameDisplay.clientWidth - 20;
+    gameDisplayYAxis = gameDisplay.clientHeight - 20;
+});
+
+//setup back home buttons
+document.querySelectorAll('.back-home-btn').forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        window.location.replace('index.html');
+    });
+});
+
+//Setup new Game --
+function startNewGame(){
+    const newGame = {
+      time: 20,
+      score: 0,
+      bullets: 5,
+      speed: 1000,
+      targets: ['good', 'bad', 'add-bullet', 'normal']
+    };
+
+    if(!game.isOver){
+
+        document.querySelector('#time').innerHTML = newGame.time;
+        document.querySelector('#score').innerHTML = newGame.score;
+        document.querySelector('#bullets').innerHTML = newGame.bullets;
+        document.querySelector('#game-over-modal').classList.add('remove');
+        document.querySelector('#game-over-modal').classList.remove('show');
+    }
+
+
+
+    //Generate random target
+    function generateRandomTargets(){
+
+        //genrate the random game.target which correspnds to the class in style.css
+        let randomTarget =  newGame.targets[Math.floor(Math.random() * 4)]; 
+
+        //generate random x and y positions
+        let randomX = Math.floor(Math.random() * (gameDisplayXAxis - 20) + 20); // numbers between 20 and x display width
+        let randomY = Math.floor(Math.random() * (gameDisplayYAxis - 20) + 20); // numbers between 20 and y display height
+        
+        //Create physical target in DOM
+        let newTarget = document.createElement('div');
+        newTarget.classList.add('target', `${randomTarget}`); //adds the target class from the generated randomTarget
+        newTarget.style.top = `${randomY}px`; // adds position Y to top style property
+        newTarget.style.left = `${randomX}px`; // adds position X to left style property
+        allTargets.appendChild(newTarget); // append newTarget to the DOM
+        
+        //Add event Listener to each target, when clicked each target will respond based on the class/game.targets
+        function hitTarget(){
+
+            //function that displays feedback from a hit target
+            function hitTargetFeedback(feedback){
+
+                let hitLabel = document.createElement('div');
+                hitLabel.classList.add('hit-label');
+                hitLabel.style.top = `${randomY}px`;
+                hitLabel.style.left = `${randomX}px`;
+                allTargets.appendChild(hitLabel);
+    
+                setTimeout(function(){
+                    allTargets.removeChild(hitLabel)
+                }, 900);
+
+                hitLabel.innerHTML = feedback
+            }
+
+            if(this.classList[1] === newGame.targets[0]){ //good target is hit
+                
+                document.querySelector('#time').innerHTML = newGame.time += 5;
+                document.querySelector('#score').innerHTML = newGame.score += 5;
+                
+                hitTargetFeedback('+5');
+
+            } else if(this.classList[1] === newGame.targets[1]) { //bad target is hit
+                
+                document.querySelector('#score').innerHTML = newGame.score -= 2;
+
+                hitTargetFeedback('-2');
+
+            } else if(this.classList[1] === newGame.targets[2]) { //addBullet target is hit
+                
+                document.querySelector('#bullets').innerHTML = newGame.bullets += 2;
+
+                // use unicode for bullet https://www.compart.com/en/unicode/U+2022
+                hitTargetFeedback('+1&#8226;');
+
+
+            } else if(this.classList[1] === newGame.targets[3]){ //Normal target is hit
+                
+                document.querySelector('#time').innerHTML = newGame.time += 1;
+                document.querySelector('#score').innerHTML = newGame.score += 1;
+
+                hitTargetFeedback('+1');
+            }
+
+            this.removeEventListener('click', hitTarget);
+        }
+        newTarget.addEventListener('click', hitTarget);
+
+        //Remove target after specified time
+        setTimeout(function(){
+            allTargets.removeChild(newTarget);
+        }, newGame.speed);
+
+    }
+    //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
+    let generatedTarget = setInterval(generateRandomTargets, newGame.speed);
+
+    //Increase level/hardness of game
+    function increaseLevel(score){
+        if(score > 59){
+            newGame.speed -= 20
+        } else if(score > 49){
+            newGame.speed -= 15;
+        } else if(score > 39){
+            newGame.speed -= 10;
+        } else if(score > 29){
+            newGame.speed -= 5;
+        } else if(score > 19){
+            newGame.speed --;
+        }
+    }
+
+    //Countdown -
+    function countDown(){
+        increaseLevel(newGame.score);
+        if(!game.isOver){
+            document.querySelector('#time').innerHTML = `${--newGame.time}`;// moved -- before variable to action decrement first 
+
+            if(newGame.time === 0){ 
+                game.isOver = true;
+            }
+
+        } else {
+
+            gameOver();
+
+        }
+    }
+    //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
+    let timer = setInterval(countDown, 1000);
+
+    //Use Bullet --
+    function fireBullet(){
+        document.querySelector('#bullets').innerHTML = `${--newGame.bullets}`;
+        if(newGame.bullets === 0){
+
+            game.isOver = true;
+
+        }
+
+        if(game.isOver){
+
+            gameOver();
+        
+        }
+    }
+    gameDisplay.addEventListener('click', fireBullet);
+
+    //function to check reason for game over
+    function gameOverReason(){
+        let reason;
+        if(newGame.time < 1){
+            reason = 'You ran out of time!'
+        } else if(newGame.bullets < 1){
+            reason = 'You ran out of bullets!'
+        } else {
+            reason = 'Try harder!'
+        }
+        return reason
+    }
+
+    //Game Over --
+    function gameOver(){
+        //clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
+        clearInterval(timer);
+        clearInterval(generatedTarget);
+
+        //removeEventListener https://www.w3schools.com/jsref/met_document_removeeventlistener.asp
+        gameDisplay.removeEventListener('click', fireBullet);
+
+        //show game over modal
+        document.querySelector('#game-over-modal').classList.add('show');
+        document.querySelector('#game-over-modal').classList.remove('remove');
+
+        //run gameOverReason function
+        document.querySelector('#game-over-reason').innerHTML = gameOverReason();
+
+        //display final score
+        // document.querySelector('.final-score span').innerHTML = newGame.score;
+        document.querySelector('#final-score-form').elements.finalScore.value = newGame.score;
+    }
+}
+
+
+// Game Menu
+function openGameMenu(){
+
+    document.querySelector('#start-game-modal').classList.add('show');
+    document.querySelector('#start-game-modal').classList.remove('remove');
+    let gameMenuBtns = document.querySelectorAll('#start-game-content button');
+    
+    //create event listener for each button on Game Menu
+    gameMenuBtns.forEach((btn) => {
+        btn.addEventListener('click', function(){
+            if(this.id === 'start-game-btn'){
+
+                document.querySelector('#start-game-modal').classList.remove('show');
+                document.querySelector('#start-game-modal').classList.add('remove');
+    
+                startNewGame();
+    
+            } else if(this.id === 'how-to-play-btn'){
+                let howToPlayBtn = document.querySelector('#how-to-play-btn');
+                //change text of the button when clicked
+                howToPlayBtn.innerHTML === 'How to play' ? howToPlayBtn.innerHTML = 'Hide' : howToPlayBtn.innerHTML = 'How to play'
+                
+                //Toggle aria-label when button is clicked
+                howToPlayBtn.getAttribute('aria-label') === 'Show how to play' ? 
+                    howToPlayBtn.setAttribute('aria-label', 'Hide how to play') :
+                    howToPlayBtn.setAttribute('aria-label', 'Show how to play')
+
+                //toggle class on instructions to show/hide instructions
+                document.querySelector('#how-to-play').classList.toggle('show-instructions');
+               
+            }
+        });
+    });
+
+}
+
+openGameMenu();
+
+
+//Restart the game
+document.querySelector('#restart-game-btn').addEventListener('click', function(){
+    game.isOver = false;
+    startNewGame();
+});
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,217 +1,210 @@
-// New game Object
-const game = {
-    isOver: false
-};
-
-// get the gameDisplay and store it as a variable
 const gameDisplay = document.querySelector('#game-display');
-//location of all targets on DOM
 const allTargets = document.querySelector('#all-targets');
+let moveTargets
+let timer
+let speed
 
-// Get X and Y axis totals for game display
-let gameDisplayXAxis = gameDisplay.clientWidth - 20;
-let gameDisplayYAxis = gameDisplay.clientHeight - 20;
+/**
+ * function that sets the speed of how quickly the targets move
+ * */ 
+function setSpeed(score){
+    speed = 1000;
+    if(score > 59){
+        speed -= 500
+    } else if(score > 49){
+        speed -= 400;
+    } else if(score > 39){
+        speed -= 300;
+    } else if(score > 29){
+        speed -= 200;
+    } else if(score > 19){
+        speed -= 100;
+    }
+    return speed
+}
 
-//update gameDisplay size when user resizes screen
-window.addEventListener('resize', function(){
-    gameDisplayXAxis = gameDisplay.clientWidth - 20;
-    gameDisplayYAxis = gameDisplay.clientHeight - 20;
-});
+/**
+ * function which is an event called when a target is hit
+ * */ 
+function hitTarget(){
+    let targetHit = this.classList[1];
+    let x = this.style.left;
+    let y = this.style.top;
 
-//setup back home buttons
-document.querySelectorAll('.back-home-btn').forEach((btn)=>{
-    btn.addEventListener('click', ()=>{
-        window.location.replace('index.html');
-    });
-});
+    let currentScore = parseInt(document.querySelector('#score').innerHTML);
+    let currentBullets = parseInt(document.querySelector('#bullets').innerHTML);
+    let currentTime = parseInt(document.querySelector('#time').innerHTML);
 
-//Setup new Game --
-function startNewGame(){
-    const newGame = {
-      time: 20,
-      score: 0,
-      bullets: 5,
-      speed: 1000,
-      targets: ['good', 'bad', 'add-bullet', 'normal']
-    };
 
-    if(!game.isOver){
+    switch(targetHit){
+        case 'good':
+            hitTargetFeedback('+5', x, y);
+            document.querySelector('#score').innerHTML = currentScore + 5;
+            document.querySelector('#time').innerHTML = currentTime + 5;
+            break;
 
-        document.querySelector('#time').innerHTML = newGame.time;
-        document.querySelector('#score').innerHTML = newGame.score;
-        document.querySelector('#bullets').innerHTML = newGame.bullets;
-        document.querySelector('#game-over-modal').classList.add('remove');
-        document.querySelector('#game-over-modal').classList.remove('show');
+        case 'bad':
+            hitTargetFeedback('-2', x, y);
+            document.querySelector('#score').innerHTML = currentScore - 2;
+            break;
+
+        case 'normal':
+            hitTargetFeedback('+1', x, y);
+            document.querySelector('#score').innerHTML = currentScore + 1;
+            document.querySelector('#time').innerHTML = currentTime + 1;
+            break;
+
+        case 'add-bullet':
+            hitTargetFeedback('+1&#8226;', x, y);
+            document.querySelector('#bullets').innerHTML = currentBullets + 2;
+            break;
+
+        default:
+            console.log('Error');
+    }          
+}
+
+/**
+ * function used to display feedback to user when a target is hit
+ * */ 
+function hitTargetFeedback(feedback, x, y){
+
+    let hitLabel = document.createElement('div');
+    hitLabel.classList.add('hit-label');
+    hitLabel.style.position = 'absolute';
+    hitLabel.style.top = y
+    hitLabel.style.left = x
+    allTargets.appendChild(hitLabel);
+
+    setTimeout(function(){
+        allTargets.removeChild(hitLabel)
+    }, 900);
+
+    hitLabel.innerHTML = feedback
+}
+
+/**
+ * function used to generate the random targets on the display
+ * */ 
+function generateRandomTargets(){
+
+    const arr = ['good','bad','add-bullet','normal'];
+    const randomTarget = arr[Math.floor(Math.random() * arr.length)];
+    const y = Math.floor(Math.random() * gameDisplay.clientHeight);
+    const x = Math.floor(Math.random() * gameDisplay.clientWidth);
+
+    //generate random target
+    let newTarget = document.createElement('div');
+    newTarget.classList.add('target', `${randomTarget}`); //adds the target class from the generated randomTarget
+    newTarget.style.top = `${y}px`; // adds position Y to top style property
+    newTarget.style.left = `${x}px`; // adds position X to left style property
+    allTargets.appendChild(newTarget); // append newTarget to the DOM
+
+    //add event listener to target
+    newTarget.addEventListener('click', hitTarget);
+
+    setTimeout(function(){
+        allTargets.removeChild(newTarget);
+        newTarget.removeEventListener('click', hitTarget)
+    }, setSpeed(document.querySelector('#score').innerHTML))
+}
+
+/**
+ * function used to deplete bullets when they are used
+ * if bullets hit zero then we call the gameOver() function
+ * */ 
+function useBullets(){
+
+    document.querySelector('#bullets').innerHTML -= 1;
+    if(bullets.innerHTML == 0){
+        gameOver();
     }
 
+}
 
-
-    //Generate random target
-    function generateRandomTargets(){
-
-        //genrate the random game.target which correspnds to the class in style.css
-        let randomTarget =  newGame.targets[Math.floor(Math.random() * 4)]; 
-
-        //generate random x and y positions
-        let randomX = Math.floor(Math.random() * (gameDisplayXAxis - 20) + 20); // numbers between 20 and x display width
-        let randomY = Math.floor(Math.random() * (gameDisplayYAxis - 20) + 20); // numbers between 20 and y display height
-        
-        //Create physical target in DOM
-        let newTarget = document.createElement('div');
-        newTarget.classList.add('target', `${randomTarget}`); //adds the target class from the generated randomTarget
-        newTarget.style.top = `${randomY}px`; // adds position Y to top style property
-        newTarget.style.left = `${randomX}px`; // adds position X to left style property
-        allTargets.appendChild(newTarget); // append newTarget to the DOM
-        
-        //Add event Listener to each target, when clicked each target will respond based on the class/game.targets
-        function hitTarget(){
-
-            //function that displays feedback from a hit target
-            function hitTargetFeedback(feedback){
-
-                let hitLabel = document.createElement('div');
-                hitLabel.classList.add('hit-label');
-                hitLabel.style.top = `${randomY}px`;
-                hitLabel.style.left = `${randomX}px`;
-                allTargets.appendChild(hitLabel);
+/**
+ * function used to create a countdown for the overall game
+ * */ 
+function countDown(){
     
-                setTimeout(function(){
-                    allTargets.removeChild(hitLabel)
-                }, 900);
-
-                hitLabel.innerHTML = feedback
-            }
-
-            if(this.classList[1] === newGame.targets[0]){ //good target is hit
-                
-                document.querySelector('#time').innerHTML = newGame.time += 5;
-                document.querySelector('#score').innerHTML = newGame.score += 5;
-                
-                hitTargetFeedback('+5');
-
-            } else if(this.classList[1] === newGame.targets[1]) { //bad target is hit
-                
-                document.querySelector('#score').innerHTML = newGame.score -= 2;
-
-                hitTargetFeedback('-2');
-
-            } else if(this.classList[1] === newGame.targets[2]) { //addBullet target is hit
-                
-                document.querySelector('#bullets').innerHTML = newGame.bullets += 2;
-
-                // use unicode for bullet https://www.compart.com/en/unicode/U+2022
-                hitTargetFeedback('+1&#8226;');
-
-
-            } else if(this.classList[1] === newGame.targets[3]){ //Normal target is hit
-                
-                document.querySelector('#time').innerHTML = newGame.time += 1;
-                document.querySelector('#score').innerHTML = newGame.score += 1;
-
-                hitTargetFeedback('+1');
-            }
-
-            this.removeEventListener('click', hitTarget);
-        }
-        newTarget.addEventListener('click', hitTarget);
-
-        //Remove target after specified time
-        setTimeout(function(){
-            allTargets.removeChild(newTarget);
-        }, newGame.speed);
-
-    }
-    //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
-    let generatedTarget = setInterval(generateRandomTargets, newGame.speed);
-
-    //Increase level/hardness of game
-    function increaseLevel(score){
-        if(score > 59){
-            newGame.speed -= 20
-        } else if(score > 49){
-            newGame.speed -= 15;
-        } else if(score > 39){
-            newGame.speed -= 10;
-        } else if(score > 29){
-            newGame.speed -= 5;
-        } else if(score > 19){
-            newGame.speed --;
-        }
-    }
-
-    //Countdown -
-    function countDown(){
-        increaseLevel(newGame.score);
-        if(!game.isOver){
-            document.querySelector('#time').innerHTML = `${--newGame.time}`;// moved -- before variable to action decrement first 
-
-            if(newGame.time === 0){ 
-                game.isOver = true;
-            }
-
-        } else {
-
-            gameOver();
-
-        }
-    }
-    //setInterval() function https://www.w3schools.com/jsref/met_win_setinterval.asp
-    let timer = setInterval(countDown, 1000);
-
-    //Use Bullet --
-    function fireBullet(){
-        document.querySelector('#bullets').innerHTML = `${--newGame.bullets}`;
-        if(newGame.bullets === 0){
-
-            game.isOver = true;
-
-        }
-
-        if(game.isOver){
-
-            gameOver();
-        
-        }
-    }
-    gameDisplay.addEventListener('click', fireBullet);
-
-    //function to check reason for game over
-    function gameOverReason(){
-        let reason;
-        if(newGame.time < 1){
-            reason = 'You ran out of time!'
-        } else if(newGame.bullets < 1){
-            reason = 'You ran out of bullets!'
-        } else {
-            reason = 'Try harder!'
-        }
-        return reason
-    }
-
-    //Game Over --
-    function gameOver(){
-        //clearInterval() function https://www.w3schools.com/jsref/met_win_clearinterval.asp
-        clearInterval(timer);
-        clearInterval(generatedTarget);
-
-        //removeEventListener https://www.w3schools.com/jsref/met_document_removeeventlistener.asp
-        gameDisplay.removeEventListener('click', fireBullet);
-
-        //show game over modal
-        document.querySelector('#game-over-modal').classList.add('show');
-        document.querySelector('#game-over-modal').classList.remove('remove');
-
-        //run gameOverReason function
-        document.querySelector('#game-over-reason').innerHTML = gameOverReason();
-
-        //display final score
-        // document.querySelector('.final-score span').innerHTML = newGame.score;
-        document.querySelector('#final-score-form').elements.finalScore.value = newGame.score;
+    if(document.querySelector('#time').innerHTML == 0){
+        gameOver();
+    } else {
+        document.querySelector('#time').innerHTML -= 1
     }
 }
 
+/**
+ * function used to start all the elements in the game
+ * */ 
+function startGame(){
 
-// Game Menu
+    document.querySelector('#bullets').innerHTML = 5;
+    document.querySelector('#time').innerHTML = 20;
+    document.querySelector('#score').innerHTML = 0;
+
+    gameDisplay.addEventListener('click', useBullets);
+    timer = setInterval(countDown, 1000);
+    moveTargets = setInterval(generateRandomTargets, setSpeed(document.querySelector('#score').innerHTML));
+    
+}
+
+/**
+ * function used to show the reason a players game is over
+ * */ 
+function gameOverReason(){
+    let reason;
+    if(document.querySelector('#time').innerHTML == 0){
+        reason = 'You ran out of time!'
+    } else if(document.querySelector('#bullets').innerHTML == 0){
+        reason = 'You ran out of bullets!'
+    } else {
+        reason = 'Try harder!'
+    }
+    return reason
+}
+
+/**
+ * function to display gameOver modal and clear all intervals
+ * */ 
+function gameOver(){
+
+    clearInterval(timer);
+    clearInterval(moveTargets);
+    gameDisplay.removeEventListener('click', useBullets);
+
+    //show game over modal
+    document.querySelector('#game-over-modal').classList.add('show');
+    document.querySelector('#game-over-modal').classList.remove('remove');
+
+    //run gameOverReason function
+    document.querySelector('#game-over-reason').innerHTML = gameOverReason();
+
+    // display final score
+    let finalSscore = document.querySelector('#score').innerHTML;
+    document.querySelector('#final-score-form').elements.finalScore.value = finalSscore;
+
+    // get Restart and Back home buttons
+    let gameOverBtns = document.querySelectorAll('#game-over-content > button');
+    
+    gameOverBtns.forEach((btn)=>{
+        btn.addEventListener('click', function(){
+
+            if(this.id === 'restart-game-btn'){
+                window.location.replace('game.html');
+
+            } else if(this.id === 'game-over-back-home'){
+                window.location.replace('index.html');
+            }
+        });
+    });
+    
+}
+
+
+/**
+ * function used to open the game menu
+ * */ 
 function openGameMenu(){
 
     document.querySelector('#start-game-modal').classList.add('show');
@@ -221,16 +214,18 @@ function openGameMenu(){
     //create event listener for each button on Game Menu
     gameMenuBtns.forEach((btn) => {
         btn.addEventListener('click', function(){
+
             if(this.id === 'start-game-btn'){
 
                 document.querySelector('#start-game-modal').classList.remove('show');
                 document.querySelector('#start-game-modal').classList.add('remove');
     
-                startNewGame();
+                startGame();
     
             } else if(this.id === 'how-to-play-btn'){
+
                 let howToPlayBtn = document.querySelector('#how-to-play-btn');
-                //change text of the button when clicked
+                //toggle text of the button when clicked
                 howToPlayBtn.innerHTML === 'How to play' ? howToPlayBtn.innerHTML = 'Hide' : howToPlayBtn.innerHTML = 'How to play'
                 
                 //Toggle aria-label when button is clicked
@@ -241,6 +236,9 @@ function openGameMenu(){
                 //toggle class on instructions to show/hide instructions
                 document.querySelector('#how-to-play').classList.toggle('show-instructions');
                
+            } else if(this.id ==='back-home'){
+
+                window.location.replace('index.html');
             }
         });
     });
@@ -248,10 +246,3 @@ function openGameMenu(){
 }
 
 openGameMenu();
-
-
-//Restart the game
-document.querySelector('#restart-game-btn').addEventListener('click', function(){
-    game.isOver = false;
-    startNewGame();
-});
