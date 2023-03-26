991a26d6f51f0b98d0a23a571b02d198e60f1e13
ofemiashiru <ofemiashiru@gmail.com>
2023-03-26 17:12:54 +0000

    fix: separate retrieving high score into its own script file as not needed in 404.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,31 +47,3 @@ window.addEventListener('resize', () => {
 
     }
 });
-
-
-// add if statement to only generate highScore if the localStorage length is greater than 0
-if(localStorage.length > 0){
-
-    // get all the high scores from local storage and parse as JSON
-    const highScores = JSON.parse(localStorage.newScore);
-
-    //Sort the high Scores
-    highScores.sort((a, b) => Object.values(b) - Object.values(a));
-
-    //get the top ten scores after they are sorted
-    const topTenScores = highScores.slice(0,10);
-
-    //generate High Scores on the leader board
-    const table = document.querySelector('#scoreboard-inner');
-
-    const scoreRows = topTenScores.map(each => {
-        for(key of Object.keys(each)){
-            return (`
-                    <div>${key.length > 10 ? key.substring(0, 10)+'...' : key}</div>
-                    <div>${each[key]}</div>`
-            );
-        }
-    });
-
-    table.innerHTML += scoreRows.join('');
-}
