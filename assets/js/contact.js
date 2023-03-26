882e43823dc8255e6a83ef84ec8b547d88f40d06
ofemiashiru <ofemiashiru@gmail.com>
2023-03-26 20:40:42 +0000

    refactor: update contact js to adhere to jshint

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,35 +1,38 @@
 emailjs.init('0Srwr_bwVJqHDTfFz');
 
 // Code for the contact form on the home page
-const sendMail = function(contactForm){
+function sendMail(e){
+    e.preventDefault();
+
     emailjs.send(
         "service_g84hf2i", "template_afgnwq7", 
         {
-            from_name: contactForm.name.value,
-            from_subject: contactForm.subject.value,
-            from_email: contactForm.emailaddress.value,
-            from_message: contactForm.message.value   
+            from_name: e.target.elements[0].value,
+            from_subject: e.target.elements[2].value,
+            from_email: e.target.elements[1].value,
+            from_message: e.target.elements[3].value   
         }, 
         "0Srwr_bwVJqHDTfFz"
     )
-    .then( () => {
+    .then( function() {
         //feedback to user that message has been sent
-        document.querySelector('#contact-section .message-feedback').innerHTML = 'Message Sent'
+        document.querySelector('#contact-section .message-feedback').innerHTML = 'Message Sent';
 
         //empty all fields once the message has been sent
-        document.querySelectorAll('.form-item').forEach((input)=>{
-            input.value =''
+        document.querySelectorAll('.form-item').forEach( function(input) {
+            input.value ='';
         });
 
         //remove the text from message feedback after 2 seconds
         setTimeout(function(){
-            document.querySelector('#contact-section .message-feedback').innerHTML = ''
+            document.querySelector('#contact-section .message-feedback').innerHTML = '';
         }, 2000);        
 
-    },(error) => {
+    },function(error) {
         console.error('error:', error);
-        document.querySelector('#contact-section .message-feedback').innerHTML = 'Something Went Wrong'
+        document.querySelector('#contact-section .message-feedback').innerHTML = 'Something Went Wrong';
     });
     
-    return false // prevents page refreshing
 }
+
+document.querySelector('#feedback-form').addEventListener('submit', sendMail);
