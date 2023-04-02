8519b60643fc3957dfbe96c84b64bea9edd896b7
ofemiashiru <ofemiashiru@gmail.com>
2023-04-02 13:03:20 +0000

    feat: sorting questions in quiz and updated style

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,13 +1,43 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
+/*
+* async function that makes call to the quiz API
+*/
+async function getQuiz(){
 
-fetch('https://opentdb.com/api.php?amount=10&category=15&type=multiple')
-.then(response => {
-    return response.json();
-})
-.then( data => {
-    console.log(data);
-})
-.catch((error) =>{
-    console.error('Error:', error);
+    let data;
+
+    const response = await fetch('https://opentdb.com/api.php?amount=10&category=15&type=multiple');
+
+    data = await response.json();
+
+    return data;
+}
+
+/**
+ * function to display the questions from the API call
+ * @param {object} quiz - the quiz response from the API call
+ */
+function displayQuestions(quiz){
+    let questions = quiz.results;
+    let count = 0;
+    
+    while(count < questions.length){
+        console.log(count + 1);
+        count++;
+    }
+}
+
+document.querySelector('#start-quiz').addEventListener('click', function(){
+    
+    document.querySelector('#quiz-inner').classList.add('hide');
+    document.querySelector('#quiz-inner-quiz').classList.remove('hide');
+
+    getQuiz()
+    .then( data => {
+        displayQuestions(data);
+    });
+
 });
+
+
