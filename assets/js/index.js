0c7186a18a52cc3f39244cf83b97e4eda803fc26
ofemiashiru <ofemiashiru@gmail.com>
2023-04-02 09:09:07 +0000

    feat: prep nav, index and style for quiz section

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,7 +40,7 @@ allNavLinks.forEach( function(link) {
 
 // closes menu when on a bigger screen and resets to burger menu
 window.addEventListener('resize', function() {
-    if(window.screen.width > 1144){
+    if(window.screen.width > 1237){
         document.querySelector('#my-nav').classList.remove('show-nav');
         openMenu.classList.remove('hide');
         openMenu.classList.add('show');
